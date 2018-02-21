960b052658fbcef71e617294355e03cea6924191
Vladimir Pryahin <pryahin.v@gmail.com>
2018-02-21 23:11:41 +0300

    Removed *.js components

--- src/components/Button/Button.js
+++
@@ -1,8 +0,0 @@
-import TopComponent from '../TopComponent/TopComponent';
-
-export default class Button extends TopComponent {
-    constructor({text, url = '', class: className = 'button'}) {
-        super('div', {class: className, 'data-url': url});
-        this.setText(text);
-    }
-}
